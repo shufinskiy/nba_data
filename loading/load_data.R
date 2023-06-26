fcb3761d68a04c52470676d4889772fb793fa65d
Vladislav Shufinskiy <hollywood.90210@mail.ru>
2023-06-26 15:16:19 +0300

    Remove stringr dependency

--- loading/load_data.R
+++ loading/load_data.R
@@ -4,7 +4,6 @@ library(httr, warn.conflicts = FALSE)
 library(jsonlite, warn.conflicts = FALSE)
 library(dplyr, warn.conflicts = FALSE)
 library(tidyr, warn.conflicts = FALSE)
-library(stringr, warn.conflicts = FALSE)
 
 source('helpers.R')
 source('utils.R')
