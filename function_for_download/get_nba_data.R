30238a54f04289c0043ceeeaab99901f08722e8e
shufinskiy <hollywood.90210@mail.ru>
2025-02-17 11:00:10 +0300

    add in_memory param in R function

--- function_for_download/get_nba_data.R
+++ function_for_download/get_nba_data.R
@@ -3,22 +3,30 @@
 #' @description
 #' Loading a nba play-by-play dataset from github repository https://github.com/shufinskiy/nba_data
 #'
-#' @param seasons Sequence or integer of the year of start of season
+#' @param seasons Sequence or integer of the year of start of season. Not used if in_memory = True
 #' @param data Sequence or string of data types to load
 #' @param seasontype Part of season: rg - Regular Season, po - Playoffs
 #' @param league Name league: NBA or WNBA
-#' @param untar do need to untar loaded archive
+#' @param in_memory Upload data without saving to hard disk
+#' @param untar do need to untar loaded archive. Not used if in_memory = True
 #' @return None
 #'
 #' @importFrom utils download.file
 #' @export
 load_nba_data <- function(path = getwd(),
-                          seasons = seq(1996, 2023),
+                          seasons = seq(1996, 2024),
                           data = c("datanba", "nbastats", "pbpstats", "shotdetail", "cdnnba", "nbastatsv3"),
                           seasontype = 'rg',
                           league = 'nba',
+                          in_memory = FALSE,
                           untar = FALSE){
-
+  
+  path <- normalizePath(path, mustWork = FALSE)
+  
+  if (length(data) > 1 & in_memory){
+    stop("Parameter in_memory=True available only when loading a single data type")
+  }
+  
   if(seasontype == 'rg'){
     df <- expand.grid(data, seasons)
     need_data <- paste(df$Var1, df$Var2, sep = "_")
@@ -33,30 +41,52 @@ load_nba_data <- function(path = getwd(),
   if(tolower(league) == 'wnba'){
     need_data <- sapply(need_data, function(x){paste0('wnba_', x)}, USE.NAMES = FALSE)
   }
-
   temp <- tempfile()
   download.file("https://raw.githubusercontent.com/shufinskiy/nba_data/main/list_data.txt", temp)
   f <- readLines(temp)
   unlink(temp)
-
   v <- unlist(strsplit(f, "="))
-
   name_v <- v[seq(1, length(v), 2)]
   element_v <- v[seq(2, length(v), 2)]
-
   need_name <- name_v[which(name_v %in% need_data)]
   need_element <- element_v[which(name_v %in% need_data)]
-
+  
   if(!dir.exists(path)){
     dir.create(path)
   }
-
+  if (in_memory){
+    df <- data.frame()
+  }
   for(i in seq_along(need_element)){
-    destfile <- paste0(path, '/', need_name[i], ".tar.xz")
-    download.file(need_element[i], destfile = destfile)
-    if(untar){
-      untar(destfile, paste0(need_name[i], ".csv"), exdir = path)
-      unlink(destfile)
+    if (in_memory){
+      temp_file <- tempfile(fileext = ".tar.xz")
+      download.file(need_element[i], destfile = temp_file, mode = "wb")
+      temp_dir <- tempdir()
+      temp_csv <- tempfile(fileext = ".csv", tmpdir = temp_dir)
+      untar(temp_file, exdir = temp_dir, files = paste0(
+        gsub(".tar.xz", "", basename(need_element[i])),".csv"
+      ))
+      csv_file <- list.files(temp_dir, pattern = "\\.csv$", full.names = TRUE)
+      if (length(csv_file) > 0) {
+        tmp_df <- read.csv(csv_file)
+        df <- rbind(df, tmp_df)
+      } else {
+        stop("No CSV file found after extraction.")
+      }
+
+      unlink(temp_file)
+      unlink(csv_file)
+      unlink(temp_dir)
+    } else {
+      destfile <- paste0(path, '/', need_name[i], ".tar.xz")
+      download.file(need_element[i], destfile = destfile)
+      if(untar){
+        untar(destfile, paste0(need_name[i], ".csv"), exdir = path)
+        unlink(destfile)
+      }
     }
+  }
+  if (in_memory){
+    return(df)
   }
 }
